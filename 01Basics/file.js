98af3f11ec5a550c20384ef5d14c86345c6c3309
Sahil Shrestha <sahiltuchhe123@gmail.com>
2024-11-21 02:25:54 +0545

    NodeJS basics

---
+++ 01Basics/file.js
@@ -0,0 +1,35 @@
+const { error } = require("console")
+const fs = require("fs")
+
+//Synchronous call
+// fs.writeFileSync("./test.txt","Hello world")
+
+
+//Asynchronous call
+// fs.writeFile("./test.txt","Hello world",(error)=>{})
+
+// Blocking and non blocking code
+
+// const result = fs.readFileSync("./contacts.txt","utf-8")
+// console.log(result)
+
+
+// const result= fs.readFileSync('./contacts.txt',"utf-8");
+// console.log(result)
+
+// fs.readFile('test.txt', 'utf8', (err, data) => {
+//     if (err) throw err;
+//     console.log(data);
+// });
+
+
+// fs.appendFileSync("./test.txt", new Date().getDate().toLocaleString())
+// File doesn't over right but it appends a new string object number or date
+
+// fs.appendFileSync("./test.txt", `${Date.now()} Hey There \n`)
+
+// fs.cpSync("./test.txt","copy.txt");
+
+fs.unlinkSync("./copy.txt");
+console.log(fs.statSync("./test.txt"))
+
---
+++ 01Basics/hello.js
@@ -0,0 +1,12 @@
+// console.log("Heyy there! I am JS")
+
+// // console.log(window);
+// // console.log(alert("hey"))
+                        
+const math = require("./math")
+// const {addFn,subFn}= require("./math")   ***************Destructuring***********
+// console.log("Math value is",math)
+
+console.log("Math value is",math.addFn(2,4))
+
+console.log("Math value is", math.subFn(6,3))
